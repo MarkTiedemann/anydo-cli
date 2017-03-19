405475e33aeeba389a11b0698684d571bb8900e1
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-03-19 01:49:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# anydo-cli
+
+**Unofficial CLI app for [Any.do](https://www.any.do/).**
+
+## Installation
+
+```
+npm install anydo --global
+```
+
+## Quickstart
+
+```
+$ anydo --help
+
+  Any.do CLI app.
+
+  - Login
+    $ anydo login --email you@example.org --password super-secret
+
+  - List your tasks
+    $ anydo [tasks]
+
+  - Logout
+    $ anydo logout
+
+```
+
+## API
+
+**See [MarkTiedemann/anydo](https://github.com/MarkTiedemann/anydo).**
+
+## License
+
+[WTFPL](http://www.wtfpl.net/) – Do What the F*ck You Want to Public License.
+
+Made with :heart: by [@MarkTiedemann](https://twitter.com/MarkTiedemannDE).
---
+++ bin.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+const anydo = require('anydo')
+const parseBody = require('anydo/lib/parse-body')
+const meow = require('meow')
+const config = new (require('conf'))()
+const updateNotifier = require('update-notifier')
+const pkg = require('./package.json')
+
+updateNotifier({ pkg }).notify()
+
+const cli = meow(`
+  - Login
+    $ anydo login --email you@example.org --password super-secret
+
+  - List your tasks
+    $ anydo [tasks]
+
+  - Logout
+    $ anydo logout
+
+`, {
+  alias: {
+    h: 'help',
+    v: 'version',
+    e: 'email',
+    p: 'password'
+  }
+})
+
+const flags = cli.flags
+
+const fail = message => {
+  console.error('Error: ' + message)
+  process.exit(1)
+}
+
+const login = () => {
+  if (!flags.email) return fail('Please specify an email via the `--email` flag')
+  if (!flags.password) return fail('Please specify a password via the `--password` flag')
+  anydo.login(flags, (err, res) => {
+    if (err) return fail(err.message)
+    config.set('anydo.auth', res.headers['x-anydo-auth'])
+  })
+}
+
+const logout = () => {
+  config.delete('anydo.auth')
+}
+
+const tasks = () => {
+  const auth = config.get('anydo.auth')
+  if (!auth) return fail('Please login first via the `login` command')
+  anydo.sync({ auth }, (err, res) => {
+    if (err) return fail(err.message)
+    parseBody(res, (err, body) => {
+      if (err) return fail(err.message)
+      body.models.task.items
+        .map(t => '- ' + t.title)
+        .forEach(t => console.log(t))
+    })
+  })
+}
+
+switch (cli.input[0]) {
+  case 'login': login(); break
+  case 'logout': logout(); break
+  case 'tasks': tasks(); break
+  default: tasks(); break
+}
